1ec018ef47e9421efad48f094b59625cb2d59581
Reinhard Loch <reinhard.loch@outlook.com>
2025-04-14 07:42:21 +0200

    final tournament settings

--- src/routes/(app)/admin/tournaments/flipfinal/[id]/settings/+page.svelte
+++ src/routes/(app)/admin/tournaments/flipfinal/[id]/settings/+page.svelte
@@ -1,5 +1,63 @@
 <script>
-	import { P } from 'flowbite-svelte';
+	import { Button, NumberInput, Label, Select } from 'flowbite-svelte';
+	import { page } from '$app/state';
+
+	let { data } = $props();
+
+	const status = data.tournament.status;
+	const settingsEnabled = status == 'Planned';
+	let originalSettings = $state(data.tournament.settings);
+	let settings = $state(data.tournament.settings);
+	let changed = $derived(JSON.stringify(settings) !== JSON.stringify(originalSettings));
+	let id = page.params.id;
+
+	let pinTypeOptions = [
+		{ value: 0, name: 'beliebige Auswahl' },
+		{ value: 1, name: 'genau ein DMD & ein Early' },
+		{ value: 2, name: 'mindestens ein DMD' }
+	];
+
+	async function updateSettings() {
+		const response = await fetch('/api/tournament/' + id, {
+			method: 'PUT',
+			body: JSON.stringify({
+				settings: settings
+			}),
+			headers: {
+				'Content-Type': 'application/json',
+				Accept: 'application/json'
+			}
+		});
+		let result = await response.json();
+		if (response.status === 200) {
+			originalSettings = JSON.parse(JSON.stringify(settings)); // triggers 'changed' update
+			data.tournament.settings = JSON.parse(JSON.stringify(settings));
+		} else {
+			alert(JSON.stringify(result));
+		}
+	}
+	const restoreSettings = () => {
+		settings = data.tournament.settings;
+	};
 </script>
 
-<P>Hier die Einstellungen des FLIP-Final-Turniers bearbeiten.</P>
+<form>
+	<div>
+		<Label>
+			<span>Wieviele Spieler sollen in die oberste Ebene aufsteigen?</span>
+			<NumberInput disabled={!settingsEnabled} min="3" max="4" bind:value={settings.numFinalists} />
+		</Label>
+		<br />
+
+		<Label>
+			Welche Flipper-Kombination soll für jedes Double-Match gelost werden?
+			<Select class="mt-2" items={pinTypeOptions} bind:value={settings.pinTypes} />
+		  </Label>
+		<br />
+
+		{#if settingsEnabled}
+			<Button disabled={!changed} on:click={updateSettings}>Speichern</Button>
+			<Button disabled={!changed} on:click={restoreSettings}>Zurücksetzen</Button>
+		{/if}
+	</div>
+</form>
