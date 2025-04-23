a70f9bf611515f320e9bf40ce85084547c3c6b4c
Reinhard Loch <reinhard.loch@outlook.com>
2025-04-23 21:36:07 +0200

    deactivate final players

--- src/routes/(app)/admin/tournaments/flipfinal/[id]/players/+page.svelte
+++ src/routes/(app)/admin/tournaments/flipfinal/[id]/players/+page.svelte
@@ -1,8 +1,18 @@
 <script>
+	import { Table, TableHead, TableBody } from 'flowbite-svelte';
+	import { TableHeadCell, TableBodyRow, TableBodyCell } from 'flowbite-svelte';
+	import { Checkbox } from 'flowbite-svelte';
 	import { invalidateAll } from '$app/navigation';
+	import { getPlayerName } from '$lib/PlayerUtil';
 	import Players from '$lib/components/Players.svelte';
 
 	let { data } = $props();
+
+	let tournament = $derived(data.tournament);
+	let settings = $derived(tournament.settings);
+	let allPlayers = $derived(data.players);
+
+	const py = 'py-1';
 
 	const description = [
 		'Hier kannst du Spieler zum Liga-Finale hinzufügen.',
@@ -30,15 +40,69 @@
 			return true;
 		}
 	}
+
+	async function updatePlayerStatus(player, active) {
+		const url = '/api/tournament/' + tournament.id;
+		const idx = settings.inactivePlayers.indexOf(player);
+		if (active) {
+			if (idx > -1) {
+				settings.inactivePlayers.splice(idx, 1);
+			}
+		} else {
+			if (idx === -1) {
+				settings.inactivePlayers.push(player);
+			}
+		}
+		const response = await fetch(url, {
+			method: 'PUT',
+			body: JSON.stringify({
+				active: active,
+				settings: settings
+			}),
+			headers: {
+				'Content-Type': 'application/json',
+				Accept: 'application/json'
+			}
+		});
+		const result = await response.json();
+		if (response.status !== 200) {
+			alert(JSON.stringify(result));
+		} else {
+			invalidateAll();
+		}
+	}
 </script>
 
-<Players
-	tournament={data.tournament}
-	tournaments={data.tournaments}
-	allPlayers={data.players}
-	{description}
-	addEnabled={data.tournament.status === 'Planned'}
-	delEnabled={data.tournament.status === 'Planned'}
-	importFrom={"flipliga"}
-	update={updatePlayers}
-/>
+{#if tournament.status === 'Planned'}
+	<Players
+		tournament={data.tournament}
+		tournaments={data.tournaments}
+		allPlayers={data.players}
+		{description}
+		addEnabled={data.tournament.status === 'Planned'}
+		delEnabled={data.tournament.status === 'Planned'}
+		importFrom={'flipliga'}
+		update={updatePlayers}
+	/>
+{:else}
+	<Table shadow hoverable={true}>
+		<TableHead>
+			<TableHeadCell>Name</TableHeadCell>
+			<TableHeadCell>Aktiv</TableHeadCell>
+		</TableHead>
+		<TableBody tableBodyClass="divide-y">
+			{#each tournament.players as player, i}
+				<TableBodyRow>
+					<TableBodyCell class={py}>{getPlayerName(player, allPlayers)}</TableBodyCell>
+					<TableBodyCell class={py}>
+						{#if !settings.inactivePlayers.includes(player)}
+							<Checkbox checked on:change={() => updatePlayerStatus(player, false)} />
+						{:else}
+							<Checkbox on:change={() => updatePlayerStatus(player, true)} />
+						{/if}
+					</TableBodyCell>
+				</TableBodyRow>
+			{/each}
+		</TableBody>
+	</Table>
+{/if}
