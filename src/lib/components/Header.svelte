bfde07d738a329888f9c2e9ebb1fa8141c5b4e57
Reinhard Loch <reinhard.loch@outlook.com>
2025-05-27 07:44:15 +0200

    access button update

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -3,6 +3,7 @@
 	import { Modal, Button, Label, Input } from 'flowbite-svelte';
 	import { HomeSolid, LockSolid, LockTimeSolid, LockOpenSolid } from 'flowbite-svelte-icons';
 	import { DarkMode } from 'flowbite-svelte';
+	import { tick } from 'svelte';
 
 	import { page } from '$app/state';
 	import { access, ReadAccess, AdminAccess } from '/src/stores.js';
@@ -16,6 +17,17 @@
 
 	let formModal = $state(false);
 	let password = $state('');
+	let passwordRef;
+
+	async function accessClicked() {
+		if (accessValue === AdminAccess) {
+			access.set(ReadAccess);
+		} else {
+			formModal = true;
+			await tick();
+			passwordRef?.focus();
+		}
+	}
 
 	function adminClicked() {
 		if (accessValue >= AdminAccess || password === import.meta.env.VITE_ADMIN_PASSWORD) {
@@ -23,12 +35,12 @@
 			password = '';
 			formModal = false;
 		} else {
+			password = '';
 			alert('Bitte gültiges Password eingeben!');
 		}
 	}
 
-	function guestClicked() {
-		access.set(ReadAccess);
+	function cancelClicked() {
 		password = '';
 		formModal = false;
 	}
@@ -48,7 +60,7 @@
 			{/each}
 		</NavUl>
 		{#if import.meta.env.VITE_APP_FULL}
-			<Button color="bg-gray-50 dark:bg-gray-800" class="!p-0" on:click={() => (formModal = true)}>
+			<Button color="bg-gray-50 dark:bg-gray-800" class="!p-0" on:click={accessClicked}>
 				{#if accessValue == ReadAccess}
 					<LockSolid class="w-5 h-5 mb-1 text-gray-500 dark:text-gray-400" />
 				{:else}
@@ -56,20 +68,26 @@
 				{/if}
 			</Button>
 		{/if}
-		<Modal title="Berechtigung wählen" bind:open={formModal} autoclose={false} class="max-w-sm">
+		<Modal
+			title="Als Administrator anmelden"
+			bind:open={formModal}
+			autoclose={false}
+			class="max-w-sm"
+		>
 			<form class="flex flex-col space-y-6" action="#">
 				<Label class="space-y-2">
-					<span>Passwort</span>
-					<Input type="password" name="password" bind:value={password} placeholder="•••••" />
+					<span>Passwort:</span>
+					<input
+						class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+						type="password"
+						name="password"
+						bind:value={password}
+						bind:this={passwordRef}
+						placeholder=""
+					/>
 				</Label>
-				<Button
-					color={accessValue == AdminAccess ? 'primary' : 'alternative'}
-					on:click={adminClicked}>Admin</Button
-				>
-				<Button
-					color={accessValue == ReadAccess ? 'primary' : 'alternative'}
-					on:click={guestClicked}>Gast</Button
-				>
+				<Button color="alternative" on:click={adminClicked}>Ok</Button>
+				<Button color="primary" on:click={cancelClicked}>Abbrechen</Button>
 			</form>
 		</Modal>
 		<DarkMode />
