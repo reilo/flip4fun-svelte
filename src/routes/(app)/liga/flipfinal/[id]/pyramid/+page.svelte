756b7280dbb07035e11264e6b8c9a7f2e543de48
Reinhard Loch <reinhard.loch@outlook.com>
2025-06-08 21:16:48 +0200

    hightlight winners

--- src/routes/(app)/liga/flipfinal/[id]/pyramid/+page.svelte
+++ src/routes/(app)/liga/flipfinal/[id]/pyramid/+page.svelte
@@ -9,8 +9,6 @@
 	let round = $derived(data.round);
 	let frames = $derived(data.frames);
 
-	const clickAreas = [];
-
 	const hOffset = 120;
 
 	let hsize = $derived(Math.min(1280, innerWidth.current));
@@ -61,11 +59,22 @@
 
 		const playingLevels = round && round.status !== 'Completed' ? round.settings.playingLevels : [];
 
+		const winners = [];
+		if (round) {
+			const results = calcFinalResults(round, frames);
+			results.levelResults.forEach((levelResult) => {
+				if (levelResult.matches == 2) {
+					winners.push(levelResult.players[0].id);
+				}
+			});
+		}
+
 		const colorLightGray = '#eeeeee';
 		const colorDarkGray = '#dddddd';
 		const colorLightGreen = '#ddeedd';
 		const colorDarkGreen = '#ccddcc';
 		const colorText = 'black';
+		const colorFrame = '#0a4f29';
 
 		let x = 0,
 			y = 0;
@@ -88,16 +97,21 @@
 				ctx.fillText((totalRows - idx).toString(), 3, rpos[idx] + imageHeight - 20);
 			}
 
-			clickAreas.splice(0, clickAreas.length);
-
 			usedPlayers.forEach((player, i) => {
 				x = hOffset + xpos[i];
 				y = ypos[i];
+				const delta = 6;
 				let img = new Image();
 				img.src = imageBaseUrl + player + imageExtension;
 				ctx.globalAlpha = activePlayers.includes(player) ? 1.0 : 0.2;
 				ctx.drawImage(img, x, y, imageWidth, imageHeight);
-				clickAreas.push({ player: player, x: x, y: y, w: imageWidth, h: imageHeight });
+				if (winners.includes(player)) {
+					ctx.strokeStyle = colorFrame;
+					ctx.lineWidth = delta;
+					ctx.beginPath();
+					ctx.rect(x + delta / 2, y + delta / 2, imageWidth - delta, imageHeight - delta);
+					ctx.stroke();
+				}
 			});
 		}, 500);
 		setTimeout(() => {
