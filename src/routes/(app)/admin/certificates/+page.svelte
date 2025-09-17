2dfe11cf2a8f6ea530f8d9e0a59e4877d6f99b06
Reinhard Loch <reinhard.loch@outlook.com>
2025-09-17 10:55:27 +0200

    urkundengenerator working

--- src/routes/(app)/admin/certificates/+page.svelte
+++ src/routes/(app)/admin/certificates/+page.svelte
@@ -1,31 +1,86 @@
 <script>
 	import { Button } from 'flowbite-svelte';
-    import { Heading } from 'flowbite-svelte';
-    import { P } from 'flowbite-svelte';
+	import { Heading } from 'flowbite-svelte';
+	import { Label } from 'flowbite-svelte';
+	import { Select } from 'flowbite-svelte';
+	import { Input } from 'flowbite-svelte';
+	import { P } from 'flowbite-svelte';
 	import { ArrowRightOutline } from 'flowbite-svelte-icons';
 	import { generateCertificatePDF } from '$lib/PDFUtil';
 
 	let { data } = $props();
 
-	const generateCertificates = () => {
-		let playerIDs = [];
+	let selPlayer = $state('');
+	let tourName = $state('Isar-Amper-Liga ' + new Date().getFullYear());
+	let rankName = $state('1. Platz');
 
-		const title = 'Liga Titel';
-		const versions = ['Aktivster Spieler'];
-		const playerNames = ['Max Mustermann'];
+	const ranks = [
+		'1. Platz',
+		'2. Platz',
+		'3. Platz',
+		'Aktivster Spieler',
+		'Aktivste Spielerin',
+		'Bester Newcomer'
+	];
+
+	const generateCertificates = () => {
+		const title = tourName;
+		const version = rankName;
+		const playerName = selPlayer
+			? playerMap.find((p) => p.value === selPlayer)?.name
+			: 'Max Mustermann';
 		const lines = ['Wir gratulieren zur hervorragenden Leistung', 'und überreichen die Urkunde an'];
 
-		generateCertificatePDF(title, versions, lines, playerNames);
+		generateCertificatePDF(title, [version], lines, [playerName]);
 	};
+
+	let playerMap = [];
+	data.players.forEach((player) => {
+		if (player.active) {
+			const name = player.forename + ' ' + player.surname;
+			playerMap.push({ name: name, value: player.id });
+		}
+	});
+	playerMap.sort((a, b) => {
+		const val = a.name > b.name ? 1 : b.name > a.name ? -1 : 0;
+		return val;
+	});
 </script>
 
 <div>
 	<Heading tag="h5">Urkunden-PDF generieren</Heading>
 
-    <P class="mb-3">Gebe die notwendigen Daten ein und generiere die Urkunde.</P>
-    
-	<Button on:click={() => generateCertificates()}>
-		Generiere PDF<ArrowRightOutline class="w-3.5 h-3.5 ml-2 text-white" />
-	</Button>
+	<P class="mb-3">Gebe die notwendigen Daten ein und generiere deine Urkunde.</P>
 
+	<div class="grid gap-6 mb-3 md:grid-cols-2">
+		<Label class="space-y-2">
+			<span>Turnier-Name</span>
+			<Input clearable bind:value={tourName} placeholder="" />
+		</Label>
+	</div>
+
+	<div class="grid gap-6 mb-3 md:grid-cols-2">
+		<Label class="space-y-2">
+			<span>Titel / Platzierung</span>
+			<Input data={ranks} clearable bind:value={rankName} placeholder="" />
+		</Label>
+	</div>
+
+	<div class="grid gap-6 mb-3 md:grid-cols-2">
+		<Label>
+			Spieler:
+			<Select
+				class="mt-4 w-40 p-3 space-y-3 text-sm"
+				items={playerMap}
+				bind:value={selPlayer}
+				placeholder="Spieler"
+			></Select>
+		</Label>
+	</div>
+
+	<div class="grid gap-6 mb-3 mt-6 md:grid-cols-2">
+		<Button on:click={() => generateCertificates()}>
+			Generiere PDF<ArrowRightOutline class="w-3.5 h-3.5 ml-2 text-white" />
+		</Button>
+	</div>
 </div>
